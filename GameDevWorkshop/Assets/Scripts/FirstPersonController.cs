97aa2c7ae16e64f0aca8c9d26c9e6094f10c7a64
Hannah <hg772921@ohio.edu>
2023-09-12 10:50:56 -0400

    9.12.23 Player Movement and Look
    Adding controls for move, look, and jump

--- GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
@@ -1,13 +1,38 @@
+//HannahGuerin 2023.9.12
+//Script for basic camera ans first person controls
+
+
+//Libraries we are using
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FirstPersonController : MonoBehaviour
 {
+
+    //Player Variables
+    public float speed = 2f;
+    public float gravity = -10f;
+    public float jumpForce = 2f;
+
+    //Movement and Looking
+    private CharacterController characterController;
+    private Vector2 moveImput;
+    private Vector3 playerVelosity;
+    private bool grounded;
+    private Vector2 mouseMovement;
+
+    //Camera Variables
+    public Camera cameraLive;
+    public float sensitivity = 25f;
+    private float camXRotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
@@ -15,4 +40,19 @@ public class FirstPersonController : MonoBehaviour
     {
         
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        
+    }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+
+    }
+    public void OnJump(InputAction.CallbackContext context) 
+    {
+    
+    }
+
+
 }
