d99f989e5349c6c23685a4f7b1392c4d661b7f40
Hannah <hg772921@ohio.edu>
2023-09-14 10:54:13 -0400

    9-14 adding more movement
    Adding more movement and looking

--- GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
@@ -33,26 +33,78 @@ public class FirstPersonController : MonoBehaviour
     void Start()
     {
       
+        characterController = GetComponent<CharacterController>();
+
+        //Hides the mouse cursor at start
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        grounded = characterController.isGrounded;
+        MovePlayer();
+        Look();
+    }
+
+    public void MovePlayer()
+    {
+       //Direction to move
+        Vector3 moveVec = transform.right * moveImput.x + transform.forward * moveImput.y;
+
+        //Move Controloler
+        characterController.Move(moveVec * speed * Time.deltaTime);
+
+        //Add Gravity
+        playerVelosity.y += gravity * Time.deltaTime;
+
+         if (grounded && playerVelosity.y < 0) {
+                     playerVelosity.y = -2.5f; 
+        }
+
+            characterController.Move(playerVelosity * Time.deltaTime);
+    }
+
+    public void Look()
+    {
+        float xAmount = mouseMovement.x * sensitivity * Time.deltaTime;
+        float yAmount = mouseMovement.y * sensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseMovement * sensitivity * Time.deltaTime);
+
+            camXRotation -= xAmount;
+            camXRotation = Mathf.Clamp(camXRotation, -90f, 90f);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        
+       
+        moveImput = context.ReadValue<Vector2>();
+        Debug.Log("Move Imput Value: " + moveImput.ToString()); 
+
     }
     public void OnLook(InputAction.CallbackContext context)
     {
 
+        mouseMovement = context.ReadValue<Vector2>();
+        Debug.Log("Mouse Movement: " + mouseMovement.ToString());
+
     }
     public void OnJump(InputAction.CallbackContext context) 
     {
-    
+
+        Jump();
+        Debug.Log("Jumped");
     }
 
+
+    public void Jump()
+    {
+        if (grounded) 
+        {
+            playerVelosity.y = jumpForce;
+        }
+    }
 
 }
